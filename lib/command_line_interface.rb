18115cbd63a79c0660a1c7b13545c0218e17c37e
Sarah <sarahyun7@gmail.com>
2019-01-03 14:28:43 -0500

    MVP

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,13 +2,12 @@ class CommandLineInterface
 
   @@apikey = 'b1eab88e7ce8602d4150d991dede49de'
 
-  def initialize
-    @zip_shelters = []
-    @shelter_array = []
-  end
-
   def greet
     puts "Welcome to Adopt-Don't-Shop!"
+  end
+
+  def create_user
+    #create new instance of user based on user input
   end
 
   def get_zip_code
@@ -44,49 +43,66 @@ class CommandLineInterface
   end
 
   def shelter_info(input)
-     @selected_shelter = @shelter_array[input-1]
+     selected_shelter = @shelter_array[input-1]
      #selects shelter name from array using input
   end
 
 
-  def get_shelter_id
-    @shelter_id = @zip_shelters.find do |shelter|
-       if shelter["name"]["$t"] == @selected_shelter
-         puts "#{shelter["id"]["$t"]}"
-       end
+  def get_shelter_id(selected_shelter)
+    found_id = @zip_shelters.find do |shelter|
+       shelter["name"]["$t"] == selected_shelter
+         # puts "#{shelter["id"]["$t"]}"
     end
+    found_id["id"]["$t"]
   end
-  #
-  #
-  # def get_type_input
-  #   puts "What kind of pet are you interested in adopting?"
-  #   puts "
-  #   1. Barnyard
-  #   2. Bird
-  #   3. Cat
-  #   4. Dog
-  #   5. Horse
-  #   6. Reptile
-  #   7. Small furry"
-  #
-  #   type_input = gets.chomp
-  # end
-  #
-  # def gets_user_pet_type(type_input)
-  #   url = "http://api.petfinder.com/#{@selected_shelter}.listByBreed?key=#{@@apikey}&animal=#{type_input}&breed=breed.list&format=json"
+
+  def get_type_input
+    puts "What kind of pet are you interested in adopting?"
+    puts "
+    1. Barnyard
+    2. Bird
+    3. Cat
+    4. Dog
+    5. Horse
+    6. Reptile
+    7. Small furry"
+
+    type_input = gets.chomp
+  end
+
+
+  def get_pet(shelter_id)
+    url = "http://api.petfinder.com/shelter.getPets?key=#{@@apikey}&id=#{shelter_id}&status=A&format=json"
+    puts url
+    response = RestClient.get url
+    response_hash = JSON.parse(response.body)
+
+    @pet_array = response_hash["petfinder"]["pets"]["pet"].collect do |pet|
+      pet["name"]["$t"]
+    end
+    binding.pry
+  end
+
+
+  # def get_pet(type_input, shelter_id)
+  #   # binding.pry
+  #   url = "http://api.petfinder.com/pet.getRandom?key=#{@@apikey}&animal=#{type_input}&shelterid=#{shelter_id}&output=basic&format=json"
   #   puts url
+  #   response = RestClient.get url
+  #   response_hash = JSON.parse(response.body)
+  #   binding.pry
   # end
+
 
   def run
     greet
     zip_code = get_zip_code
     @shelter_array = find_shelters(zip_code)
     input = get_user_input
-    @selected_shelter = shelter_info(input)
-    @shelter_id = get_shelter_id
-    # type_input = get_type_input
-    # gets_user_pet_type(type_input)
-    # binding.pry
+    selected_shelter = shelter_info(input)
+    shelter_id = get_shelter_id(selected_shelter)
+    type_input = get_type_input
+    get_pet(shelter_id)
   end
 
 
